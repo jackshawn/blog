9741ab49571d0d01475242fec3ede722752fca50
jackshawn <jackshawn@live.com>
2018-05-23 18:06:23 +0800

    add new component

---
+++ src/components/More.vue
@@ -0,0 +1,74 @@
+<template>
+  <p id="fetch">
+    <span v-if="state ==='loaded'" @click="getMore" class="more">MORE</span>
+    <span v-if="state ==='loading'" class="loading">LOADING..<span class="dot">.</span></span>
+    <span v-if="state ==='done'" class="done">DONE</span>
+  </p>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    name: 'More',
+    props: {
+      nav: String
+    },
+    data() {
+      return {
+        state: 'loaded',
+        startDate: ''
+      }
+    },
+    mounted() {
+      this.getMore()
+    },
+    methods: {
+      getMore() {
+        let _this = this;
+        let url = (this.nav === 'Blog' ? '/blog' : '/photo/' + this.nav.toString().toLowerCase()) + (this.startDate ? '?startDate=' + this.startDate : '');
+
+        this.state = 'loading'
+        axios.get(url)
+          .then((res) => {
+            let d = res.data;
+            _this.state = d.length < 6 ? 'done' : 'loaded';
+            if(d.result === 'success') {
+              _this.$emit('fetch', d.data)
+            }
+          }).catch((error) => {
+          console.log(error)
+        });
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #fetch {
+    text-align: center;
+    margin: 10px 0 20px;
+  }
+
+  #fetch .more {
+    cursor: pointer;
+  }
+
+  #fetch .dot {
+    animation: blink 1s;
+    animation-iteration-count: infinite;
+  }
+
+  @keyframes blink {
+    0% {
+      opacity: 0;
+    }
+
+    49% {
+      opacity: 0;
+    }
+
+    50% {
+      opacity: 1;
+    }
+  }
+</style>
---
+++ src/components/Subtitle.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="subtitles">
+    <span class="left">“</span>
+    <template v-for="item in subtitles[nav].split(',')">
+      <p>{{item}}</p>
+    </template>
+    <span class="right">“</span>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      nav: String,
+    },
+    data() {
+      return {
+        subtitles: {
+          Home: "记录生活,无关技术",
+          Blog: "一直有写写东西的习惯,择一些能贱人的放在这边",
+          Life: "x,x",
+          Movie: "x,x",
+          Hehe: "x,x",
+          Drawing: "x,x"
+        }
+      }
+    },
+  }
+</script>
+
+<style scoped>
+  #subtitles {
+    font-family: Helvetica, Arial, sans-serif, 'Avenir';
+    font-size: 14px;
+    line-height: 3em;
+    margin: 0 auto;
+    padding: 20px 40px;
+    position: relative;
+    text-align: center;
+    max-width: 520px;
+  }
+
+  #subtitles .left,
+  #subtitles .right {
+    font-size: 36px;
+    font-weight: bold;
+    display: inline-block;
+    position: absolute;
+  }
+
+  #subtitles .left {
+    top: 0;
+    left: 20px;
+  }
+
+  #subtitles .right {
+    bottom: 0;
+    right: 20px;
+    transform: rotate(180deg);
+  }
+</style>
