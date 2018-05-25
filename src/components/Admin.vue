f5f8adf4ffae5ba24ab9b85e10613f0ef0ce8370
jackshawn <jackshawn@live.com>
2018-05-25 17:29:52 +0800

    add component

---
+++ src/components/Admin.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="height: 100%;">
+    welcome
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Admin',
+    data() {
+      return {
+
+      }
+    }
+  }
+</script>
+
---
+++ src/components/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <div style="height: 100%;">
+    <br>
+    <input type="text" v-model="username">
+    <br>
+    <br>
+    <input type="password" v-model="password">
+    <br>
+    <br>
+    <button>Login</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Login',
+    data() {
+      return {
+        username: '',
+        password: ''
+      }
+    }
+  }
+</script>
+
