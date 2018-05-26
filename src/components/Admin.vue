ffd1cf299345bedbd22158d9f27433bd7a016c0b
jackshawn <jackshawn@live.com>
2018-05-26 15:35:13 +0800

    update admin

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,15 +1,48 @@
 <template>
   <div style="height: 100%;">
     welcome
+
+    <br>
+    <br>
+    <br>
+    <button @click="logout"></button>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
   export default {
     name: 'Admin',
     data() {
-      return {
-
+      return {}
+    },
+    mounted() {
+      this.check();
+    },
+    methods: {
+      check() {
+        let _this = this;
+        axios.get('/check')
+          .then((res) => {
+            if(res.data.result === 'fail') {
+              _this.$router.push('Login')
+            }
+          }).catch((error) => {
+          console.log(error)
+        });
+      },
+      logout() {
+        let _this = this;
+        axios.get('/logout')
+          .then((res) => {
+            if(res.data.result === 'success') {
+              _this.$router.push('Login')
+            } else {
+                alert(res.data.msg)
+            }
+          }).catch((error) => {
+          console.log(error)
+        });
       }
     }
   }
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -7,17 +7,50 @@
     <input type="password" v-model="password">
     <br>
     <br>
-    <button>Login</button>
+    <button @click="login">Login</button>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
   export default {
     name: 'Login',
     data() {
       return {
         username: '',
         password: ''
+      }
+    },
+    mounted() {
+      this.check();
+    },
+    methods: {
+      check() {
+        let _this = this;
+        axios.get('/check')
+          .then((res) => {
+            if(res.data.result === 'success') {
+              _this.$router.push('Admin')
+            }
+          }).catch((error) => {
+          console.log(error)
+        });
+      },
+      login() {
+        let _this = this;
+        axios.post('/login', {
+          username: _this.username,
+          password: _this.password
+        })
+          .then((res) => {
+            if(res.data.result === 'success') {
+              this.$router.push('Admin')
+            } else {
+              alert(res.data.msg)
+            }
+          }).catch((error) => {
+          console.log(error)
+        });
       }
     }
   }
